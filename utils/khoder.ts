ba1e473ca272087c0814675a3720c2f9e3b5e4b1
mahmoudkhoder22m <mahmoudkhoder22m@gmail.com>
2025-10-24 00:56:45 +0300

    test-3 (#1)

--- utils/khoder.ts
+++ utils/khoder.ts
@@ -3,3 +3,4 @@ const age=25;
 const job='developer';
 const city='cairo';
 console.log(`hello ${name} you are ${age} years old you work as ${job} in ${city}`);
+console.log(`hello ${name} you are ${age} years old you work as ${job} in ${city}`);
